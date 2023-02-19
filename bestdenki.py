f5923555302fd52cb36a79cf0a1e06c9e388345b
iamnotkenneth <88260663+iamnotkenneth@users.noreply.github.com>
2023-02-19 10:29:30 +0800

    Update bestdenki.py

--- bestdenki.py
+++ bestdenki.py
@@ -1,33 +1,43 @@
+import requests
 import httpx
 from selectolax.parser import HTMLParser
 from bs4 import BeautifulSoup
-import requests
+
 response = ""
-search_item = input("Search an item: ")
-best_instant = "https://www.bestdenki.com.sg/instantsearch/result/?q={}".format(search_item) 
-response = requests.get(best_instant)
-response = response.url
-name_selector = "div.webmodeldescription"
-price_selector = "span.price"
+#search_item = input("Search an item: ")
+denki_list = []
 
-
-def get_data(url,name_selector,price_selector,):
+def get_data(search_item):
+    best_instant = "https://www.bestdenki.com.sg/instantsearch/result/?q={}".format(search_item) #generate generic link
+    response = requests.get(best_instant) #get redirected link
+    response = response.url #url
+    name_selector = "div.webmodeldescription" 
+    price_selector = "span.price"
     resp = httpx.get(
-        url,
+        response,
         headers={
             "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
         }
     )
     html = HTMLParser(resp.text)
     #GET NAME & PRICE
-    name = html.css_first(name_selector).text().strip()
-    price = html.css_first(price_selector).text().strip()
+    name_list = html.css(name_selector)
+    price_list = html.css(price_selector)
     #GET LINK
+    #HTML PARSER
     soup = BeautifulSoup(resp, 'html.parser')
-    # Find the first anchor tag with class="product-item-link"
-    product_link = soup.find('a', {'class': 'product-item-link'})
-    # Extract the href attribute value
-    link = product_link['href']
-    return {'name ': name,'price ' : price,'link ': link}
+    # Find the first image link that has the class="product-item-link"
+    link_list = (soup.find_all('a', {'class': 'product-item-link'}))
+    limit = 5
+    for x in link_list:
+        if limit > 0:
+            name = name_list[(5-limit)].text().strip()
+            price = price_list[(5-limit)].text().strip()
+            limit -= 1
+            link = x['href']
+            denki_list.append( {'name ': name,'price ' : price,'link ': link,})
+        else: 
+            break
+    return denki_list
 
-print(get_data(response,name_selector,price_selector))
+print(get_data("iPhone 13"))
