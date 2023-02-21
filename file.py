16bbe7da0c857c46c603b26a66fa191723ac0c63
iamnotkenneth <88260663+its-kenneth@users.noreply.github.com>
2023-02-21 10:03:44 +0800

    Update file.py

--- file.py
+++ file.py
@@ -3,11 +3,10 @@ import httpx
 import random 
 from selectolax.parser import HTMLParser
 from bs4 import BeautifulSoup
-
-response = ""
 #search_item = input("Search an item: ")
 
 def get_norman(search_item):    
+    response = ""
     norman_link = "https://www.harveynorman.com.sg/index.php?subcats=Y&status=A&pshort=N&pfull=N&pname=Y&pkeywords=Y&search_performed=Y&q={}+14&dispatch=products.search".format(search_item) #generate generic link
     #response = requests.get(best_instant) #get redirected link
     #response = response.url #url
@@ -28,26 +27,28 @@ def get_norman(search_item):
     soup = BeautifulSoup(resp, 'html.parser')
     # Find the first image link that has the class="product-item-link"
     link_list = (soup.find_all('a', {'class': 'btn btn-action btn_sm'}))
-    if len(link_list) > 5:
+    if min(len(link_list),len(name_list),len(price_list)) > 5:
         limit = 5
     else: 
-        limit = len(link_list)
+        limit = min(len(link_list),len(name_list),len(price_list))
     norman_list = []
-    for x in link_list:
-        if limit > 0:
-            name = name_list[(5-limit)].text().strip()
-            price = price_list[(5-limit)].text().strip()
-            limit -= 1
-            link = (x['href']).replace('//www','www')
-            norman_list.append( {'name ': name,'price ' : price.replace('S',''),'link ': link,})
-        else: 
-            break
+    if limit > 0:
+        for x in link_list:
+            if limit > 0:
+                name = name_list[(5-limit)].text().strip()
+                price = price_list[(5-limit)].text().strip()
+                limit -= 1
+                link = (x['href']).replace('//www','www')
+                norman_list.append( {'name ': name,'price ' : price.replace('S',''),'link ': link,})
+            else: 
+                break
+    elif limit == 0:
+        norman_list = []    
     return norman_list
 
-
-response = ""
 #search_item = input("Search an item: ")
 def get_denki(search_item):
+    response = ""
     best_instant = "https://www.bestdenki.com.sg/instantsearch/result/?q={}".format(search_item) #generate generic link
     response = requests.get(best_instant) #get redirected link
     response = response.url #url
@@ -68,20 +69,23 @@ def get_denki(search_item):
     soup = BeautifulSoup(resp, 'html.parser')
     # Find the first image link that has the class="product-item-link"
     link_list = (soup.find_all('a', {'class': 'product-item-link'}))
-    if len(link_list) > 5:
+    if min(len(link_list),len(name_list),len(price_list)) > 5:
         limit = 5
     else: 
-        limit = len(link_list)
+        limit = min(len(link_list),len(name_list),len(price_list))
     denki_list = []
-    for x in link_list:
-        if limit > 0:
-            name = name_list[(5-limit)].text().strip()
-            price = price_list[(5-limit)].text().strip()
-            limit -= 1
-            link = x['href']
-            denki_list.append( {'name ': name,'price ' : price,'link ': link,})
-        else: 
-            break
+    if limit > 0:
+        for x in link_list:
+            if limit > 0:
+                name = name_list[(5-limit)].text().strip()
+                price = price_list[(5-limit)].text().strip()
+                limit -= 1
+                link = (x['href']).replace('//www','www')
+                denki_list.append( {'name ': name,'price ' : price.replace('S',''),'link ': link,})
+            else: 
+                break
+    elif limit == 0:
+        denki_list = []  
     return denki_list
 
 cheapest_info = []
@@ -89,17 +93,21 @@ def get_cheapest(search_item):
     all = get_norman(search_item) + get_denki(search_item)
     cheapest_info = []
     #print(all)
-    cheapest_item = all[random.randint(0,len(all)-1)]
-    for z in all:
-        price = float(z['price '].replace('$', '').replace(',',''))
-        cheapest_price = float(cheapest_item['price '].replace('$', '').replace(',',''))
-        if price < cheapest_price:
-            cheapest_item = z
-    cheapest_info.append(cheapest_item['name '])
-    cheapest_info.append(float(cheapest_price))
-    cheapest_info.append(cheapest_item['link '])
-    return cheapest_info
+    if len(all)>0:
+        cheapest_item = all[random.randint(0,len(all)-1)]
+        for z in all:
+            price = float(z['price '].replace('$', '').replace(',',''))
+            cheapest_price = float(cheapest_item['price '].replace('$', '').replace(',',''))
+            if price < cheapest_price:
+                cheapest_item = z
+        cheapest_info.append(cheapest_item['name '])
+        cheapest_info.append(float(cheapest_price))
+        cheapest_info.append(cheapest_item['link '])
+        return cheapest_info
+    return "Please enter another item"
 
-print(get_cheapest("Samsung Galaxy"))
-print(get_cheapest("iPhone 14"))
-print(get_cheapest("TV"))
+print(get_cheapest("b"))
+#print(get_cheapest("Air Conditioner"))
+#print(get_cheapest("Samsung Galaxy"))
+#print(get_cheapest("iPhone 14"))
+#print(get_cheapest("TV"))
